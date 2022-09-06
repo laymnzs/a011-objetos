12897edfb2e944f3e851fed5e55ff646c51ab6ee
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2022-09-06 18:28:07 -0300

    Exercício Objetos a011

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+//a) 
+ const fruta1 = {
+    nome1: "uva",
+    preco1: 4.40,
+    disponibilidade: false 
+}
+
+ const fruta2 = {
+    nome2: "banana",
+    preco2: 6.25,
+    disponibilidade: true
+}
+
+const fruta3 = {
+    nome3: "morango",
+    preco3: 2.49,
+    disponibilidade: false 
+}
+
+//b) 
+sacolao.push(fruta1, fruta2, fruta3)
+console.log(sacolao)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,16 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+let ator = filme.elenco;
+
+//a)
+console.log(ator[0]);
+
+//b)
+console.log(ator[3]); 
+
+//c)
+console.log(filme.transmissoesHoje); 
+
+//d)
+console.log(filme.transmissoesHoje[1].horario); 
